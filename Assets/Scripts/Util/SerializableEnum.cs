24f03900804731de5a76d59c77f2661b7df2e87d
Spencer Shustitzky Sohn <sohn.spencer@gmail.com>
2018-02-05 15:43:26 -0700

    no message

--- Assets/Scripts/Util/SerializableEnum.cs
+++ Assets/Scripts/Util/SerializableEnum.cs
@@ -1,7 +1,18 @@
-﻿namespace Util
+﻿using System;
+using UnityEngine;
+
+namespace Util
 {
-    public class SerializableEnum
+    [Serializable]
+    public class SerializableEnum<T> where T : struct, IConvertible
     {
-        
+        public T Value
+        {
+            get { return _enumValue; }
+            set { _enumValue = value; }
+        }
+
+        [SerializeField] private string _enumValueAsString;
+        [SerializeField] private T _enumValue;
     }
 }
