50eaf121647e41aa264f6af6fc2efc1e7cc85efd
jmbickel <sphinx527@comcast.net>
2018-11-01 11:25:31 -0400

    My little not and awkward unity sketch :D

--- Assets/Scripts/Managers/Outputs/Building/BaseBuildingDisplayScript.cs
+++ Assets/Scripts/Managers/Outputs/Building/BaseBuildingDisplayScript.cs
@@ -10,7 +10,7 @@ namespace Managers.Outputs.Building
 {
     public class BaseBuildingDisplayScript : MonoBehaviour
     {
-        public GameObject DisplayName, BuildingLevel;
+        public GameObject BuildingLevel;
         public Button PurchaseButton;
         public BuildingEnum BuildingEnum;
         protected BuildingType BuildingType;
@@ -32,9 +32,8 @@ namespace Managers.Outputs.Building
         {
             if (info.BuildingType != BuildingType)
             {
-                throw new Exception("BUILDING:" + DisplayName + " is not of type " + BuildingType);
+                throw new Exception("BUILDING:" + info.Name + " is not of type " + BuildingType);
             }
-            SetValue(info.Name, DisplayName);
             SetValue(info.Level, BuildingLevel);
         }
 
